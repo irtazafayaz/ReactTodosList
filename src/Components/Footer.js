e9839bfa69a13f8272613ac887e604d4272a40fa
Irtaza Fiaz <irtazafayaz@gmail.com>
2022-08-09 00:15:46 +0500

    File Structure

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Footer View Rendered!
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Todos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+const Todos = () => {
+  return (
+    <TodoItem/>
+  )
+}
+
+export default Todos
